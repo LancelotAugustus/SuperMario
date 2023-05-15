f1edb534541c3a3289a067d4084fcfa08087049a
LancelotAugustus <Lancelot_Augustus@outlook.com>
2023-05-15 20:02:28 +0800

    Version 0.5 1.在main_menu.py中定义MainMenu类。 2.在MainMenu类中新增setup_background方法，用以设置背景图片。 3.在ainMenu类中新增setup_player方法，用以设置玩家图片。 4.在ainMenu类中新增setup_cursor方法，用以设置光标图片。 5.在ainMenu类中新增update方法，用以更新屏幕。 6.在setup.py中新增SCREEN常量，表示屏幕宽高。 7.在constants.py中新增BG_MULTI常量，表示背景缩放比例。 8.在constants.py中新增PLAYER_MULTI常量，表示玩家缩放比例。 9.移除tools.py中Game类的run方法中的测试代码，并更新游戏背景。 10.在main.py中新增主页面状态机，并更新run方法参数。

--- source/setup.py
+++ source/setup.py
@@ -4,6 +4,6 @@ from . import tools
 
 
 pygame.init()
-pygame.display.set_mode((C.SCREEN_W, C.SCREEN_H))
+SCREEN = pygame.display.set_mode((C.SCREEN_W, C.SCREEN_H))
 
 GRAPHICS = tools.load_graphics('resources/graphics')
