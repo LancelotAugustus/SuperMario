5f1eb52b582c94e3123da65345ecde7a954ac056
LancelotAugustus <Lancelot_Augustus@outlook.com>
2023-05-16 16:44:31 +0800

    Version 0.11 1.在player.py中调整Player类。 2.在Player类中新增load_data方法用以载入json数据。 3.在Player类中调整setup_state方法，添加人物状态机。 4.在Player类中移除前一版本中setup_state方法的测试代码。 5.在Player类中调整load_images方法的帧框架。 6.在Player类中移除前一版本中update方法的测试代码。 7.在Player类中调整update方法，添加人物状态机。

--- source/components/player.py
+++ source/components/player.py
@@ -1,4 +1,6 @@
 import pygame
+import json
+import os
 from .. import setup, tools
 from .. import constants as C
 
@@ -7,12 +9,20 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, name):
         pygame.sprite.Sprite.__init__(self)
         self.name = name
+        self.load_data()
         self.setup_state()
         self.setup_velocities()
         self.setup_timers()
         self.load_images()
 
+    def load_data(self):
+        file_name = self.name + '.json'
+        file_path = os.path.join('source/data/player', file_name)
+        with open(file_path) as f:
+            self.player_data = json.load(f)
+
     def setup_state(self):
+        self.state = 'walk'
         self.face_right = True
         self.dead = False
         self.big = False
@@ -27,20 +37,32 @@ class Player(pygame.sprite.Sprite):
 
     def load_images(self):
         sheet = setup.GRAPHICS['mario_bros']
-        self.right_frames = []
-        self.left_frames = []
-        self.up_frames = []
-        self.down_frames = []
-        frame_rects = [(178, 32, 12, 16), (80, 32, 15, 16), (96, 32, 16, 16), (112, 32, 16, 16)]
-        for frame_rect in frame_rects:
-            right_frame = tools.get_image(sheet, *frame_rect, (0, 0, 0), C.PLAYER_MULTI)
-            left_frame = pygame.transform.flip(right_frame, True, False)
-            up_frame = pygame.transform.rotate(right_frame, 90)
-            down_frame = pygame.transform.rotate(right_frame, - 90)
-            self.right_frames.append(right_frame)
-            self.left_frames.append(left_frame)
-            self.up_frames.append(up_frame)
-            self.down_frames.append(down_frame)
+        frame_rects = self.player_data['image_frames']
+        self.right_small_normal_frames = []
+        self.right_big_normal_frames = []
+        self.right_big_fire_frames = []
+        self.left_small_normal_frames = []
+        self.left_big_normal_frames = []
+        self.left_big_fire_frames = []
+        self.small_normal_frames = [self.right_small_normal_frames, self.left_small_normal_frames]
+        self.big_normal_frames = [self.right_big_normal_frames, self.left_big_normal_frames]
+        self.big_fire_frames = [self.right_big_fire_frames, self.left_big_fire_frames]
+        self.all_frames = [self.right_small_normal_frames, self.right_big_normal_frames, self.right_big_fire_frames, self.left_small_normal_frames, self.left_big_normal_frames, self.left_big_fire_frames]
+        self.right_frames = self.right_small_normal_frames
+        self.left_frames = self.left_small_normal_frames
+        for group, group_frame_rects in frame_rects.items():
+            for frame_rect in group_frame_rects:
+                right_image = tools.get_image(sheet, frame_rect['x'], frame_rect['y'], frame_rect['width'], frame_rect['height'], (0, 0, 0), C.PLAYER_MULTI)
+                left_image = pygame.transform.flip(right_image, True, False)
+                if group == 'right_small_normal':
+                    self.right_small_normal_frames.append(right_image)
+                    self.left_small_normal_frames.append(left_image)
+                elif group == 'right_big_normal':
+                    self.right_big_normal_frames.append(right_image)
+                    self.left_big_normal_frames.append(left_image)
+                elif group == 'right_big_fire':
+                    self.right_big_fire_frames.append(right_image)
+                    self.left_big_fire_frames.append(left_image)
         self.frame_index = 0
         self.frames = self.right_frames
         self.image = self.frames[self.frame_index]
@@ -49,23 +71,23 @@ class Player(pygame.sprite.Sprite):
     def update(self, keys):
         self.current_time = pygame.time.get_ticks()
         if keys[pygame.K_RIGHT]:
+            self.state = 'walk'
             self.x_vel = 5
             self.y_vel = 0
             self.frames = self.right_frames
-        elif keys[pygame.K_LEFT]:
+        if keys[pygame.K_LEFT]:
+            self.state = 'walk'
             self.x_vel = - 5
             self.y_vel = 0
             self.frames = self.left_frames
-        elif keys[pygame.K_UP]:
-            self.x_vel = 0
+        if keys[pygame.K_SPACE]:
             self.y_vel = - 5
-            self.frames = self.up_frames
-        elif keys[pygame.K_DOWN]:
-            self.frames = self.down_frames
-            self.x_vel = 0
-            self.y_vel = 5
-        if self.current_time - self.walking_timer > 100:
-            self.walking_timer = self.current_time
-            self.frame_index += 1
-            self.frame_index %= 4
+            self.state = 'jump'
+        if self.state == 'walk':
+            if self.current_time - self.walking_timer > 100:
+                self.walking_timer = self.current_time
+                self.frame_index += 1
+                self.frame_index %= 4
+        if self.state == 'jump':
+            self.frame_index = 4
         self.image = self.frames[self.frame_index]
